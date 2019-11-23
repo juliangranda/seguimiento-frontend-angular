4100dedfd5ba8cf8d15c2363fafc0abb6774f704
juliangranda <juliangranda@hotmail.com>
2019-11-23 16:27:05 -0500

    empezando validadores

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,9 +14,13 @@ import { PersonaComponent } from './components/persona/persona.component';
 import { EmpleadorComponent } from './components/empleador/empleador.component';
 
 
+//componente validador
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 @NgModule({
   declarations: [AppComponent, NavbarComponent, PersonaComponent, EmpleadorComponent],
-  imports: [BrowserModule, MDBBootstrapModule.forRoot(), NavbarModule, AppRoutingModule],
+  imports: [BrowserModule, MDBBootstrapModule.forRoot(), NavbarModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- frontend/src/app/components/persona/persona.component.ts
+++ frontend/src/app/components/persona/persona.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import{FormGroup, FormControl, Validators} from '@angular/forms';
 @Component({
   selector: 'app-persona',
   templateUrl: './persona.component.html',
@@ -7,8 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonaComponent implements OnInit {
 
-  constructor() { }
-
+  forma:FormGroup;
+  constructor() { 
+    this.forma = new FormGroup({
+      //valor varible default/validadores/validadores asincronos
+      'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
+      'apellido': new FormControl('', Validators.required),
+      'correo': new FormControl('', [ Validators.required, 
+                                      Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")])
+    })
+  }
+  guardarCambios(){
+    console.log(this.forma);
+  }
   ngOnInit() {
   }
 
